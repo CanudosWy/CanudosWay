5d2d09b51e19affdb2c09e498a8d542dfd0359fd
Fernando Silva <fernando.debrando@gmail.com>
2014-09-02 21:46:42 -0300

    Tela de login

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost:81/CanudosWay';
+$config['base_url']	= 'http://localhost:81/CanudosWay';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'localhost:81/CanudosWay';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/login.php
+++ application/views/login.php
@@ -3,78 +3,45 @@
 <head>
   <meta charset="utf-8">
   <title>Canudo's Way - Login</title>
-
-  <style type="text/css">
-
-  ::selection{ background-color: #E13300; color: white; }
-  ::moz-selection{ background-color: #E13300; color: white; }
-  ::webkit-selection{ background-color: #E13300; color: white; }
-
-  body {
-    background-color: #fff;
-    margin: 40px;
-    font: 13px/20px normal Helvetica, Arial, sans-serif;
-    color: #4F5155;
-  }
-
-  a {
-    color: #003399;
-    background-color: transparent;
-    font-weight: normal;
-  }
-
-  h1 {
-    color: #444;
-    background-color: transparent;
-    border-bottom: 1px solid #D0D0D0;
-    font-size: 19px;
-    font-weight: normal;
-    margin: 0 0 14px 0;
-    padding: 14px 15px 10px 15px;
-  }
-
-  code {
-    font-family: Consolas, Monaco, Courier New, Courier, monospace;
-    font-size: 12px;
-    background-color: #f9f9f9;
-    border: 1px solid #D0D0D0;
-    color: #002166;
-    display: block;
-    margin: 14px 0 14px 0;
-    padding: 12px 10px 12px 10px;
-  }
-
-  #body{
-    margin: 0 15px 0 15px;
-  }
-
-  p.footer{
-    text-align: right;
-    font-size: 11px;
-    border-top: 1px solid #D0D0D0;
-    line-height: 32px;
-    padding: 0 10px 0 10px;
-    margin: 20px 0 0 0;
-  }
-
-  #container{
-    margin: 10px;
-    border: 1px solid #D0D0D0;
-    -webkit-box-shadow: 0 0 8px #D0D0D0;
-  }
-  </style>
+  <link href="<?php echo base_url('application/assets/css/bootstrap.min.css');?>" rel="stylesheet" media="screen">
+  <link href="<?php echo base_url('application/assets/css/style.css');?>" rel="stylesheet" media="screen">
+  <script src="<?php echo base_url('application/assets/js/jquery-2.1.1.min.js');?>" ></script>
+  <script src="<?php echo base_url('application/assets/js/bootstrap.min.js');?>" ></script>
 </head>
 <body>
 
-<div id="container">
-  <h1>Bem vindo ao Canudo's Way!</h1>
+  <div id="container">
+    <div class="col-sm-8 centered">
+      <h1>Bem vindo ao Canudo's Way!</h1>
 
-  <div id="body">
-    <p>Canudo's Way é um aplicativo que possibilita você planejar o melhor caminho para concluir o seu curso da Faculdade Senac.</p>
+      <div id="body">
+        <p>Canudo's Way é um aplicativo que possibilita você planejar o melhor caminho para concluir o seu curso da Faculdade Senac.</p>
+      </div>
+      <div class="col-sm-4 centered">
+        <form class="form-horizontal">
+          <div class="control-group">
+            <label class="control-label" for="inputEmail">E-mail</label>
+            <div class="controls">
+              <input id="inputEmail" type="text" placeholder="Digite o seu e-mail..." />
+            </div>
+          </div>
+          <div class="control-group">
+            <label class="control-label" for="inputPassword">Senha</label>
+            <div class="controls">
+              <input id="inputPassword" type="password" placeholder="Digite a sua senha..." />
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="controls">
+              <label class="checkbox"><input type="checkbox" /> Lembrar senha</label>
+              <button class="btn" type="submit">Acessar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <p class="footer">Desenvolvido por <strong>Canudo's Way</strong>.</p>
+    </div>
   </div>
-
-  <p class="footer">Desenvolvido por <strong>Canudo's Way</strong>.</p>
-</div>
 
 </body>
 </html>
