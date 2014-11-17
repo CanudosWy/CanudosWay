f93a7aefb27c5a409549f100945e4ecbbdbbc320
CanudosWay <canudosway@gmail.com>
2014-11-17 19:27:07 -0200

    Update grade.js

--- assets/js/grade.js
+++ assets/js/grade.js
@@ -1,4 +1,103 @@
 
+$(document).ready(function() {
+
+
+	$("select").multiselect();
+
+	$('#filtroTitulo').on("click", function() {
+
+		$('#divFiltro').fadeToggle("slow", "linear");
+
+	});
+	
+
+	$("#accordion" ).accordion({heightStyle: "content"});
+
+	$(".ulLista1,.dropSegunda").sortable({
+		connectWith: ".dropSegunda,.ulLista1",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+	$(".ulLista2,.dropTerca").sortable({
+		connectWith: ".dropTerca,.ulLista2",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+	$(".ulLista3,.dropQuarta").sortable({
+		connectWith: ".dropQuarta,.ulLista3",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+	$(".ulLista4,.dropQuinta").sortable({
+		connectWith: ".dropQuinta,.ulLista4",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+	$(".ulLista5,.dropSexta").sortable({
+		connectWith: ".dropSexta,.ulLista5",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+	$(".ulLista6,.dropSabado").sortable({
+		connectWith: ".dropSabado,.ulLista6",
+		dropOnEmpty: true,
+		receive: function(event, ui) {
+			var list = $(this);
+			if (list.children().length > 1) {
+				alert("Selecione apenas uma cadeira!");
+						// enviar o item para o lugar que estava
+						$(ui.sender).sortable('cancel');
+					}
+					
+				}
+			});
+
+	$(".ulListaSegunda,.dropSegunda").disableSelection();
+
+	Filtros();
+	
+});
+
 function avaliarDisciplina($id){
 
 	var request = $.ajax({
@@ -25,4 +124,15 @@ function avaliarDisciplina($id){
 	$('#conteudoAvaliacao').load("http://localhost:8070/CanudosWay/home/votacao");
 	$('#modalAvaliarDisciplina').modal();
 
+}
+
+function Filtros(){
+
+	$("input[name='multiselect_diaSemana']").click(function(){ 
+
+		$("input[name='multiselect_diaSemana']").each(function(index, element){
+			console.log(element);
+		});
+
+	});
 }
